3aeeb6fcde9a6ccfc5351cd9ce6f6311a6d3df27
Dsibe <42495727+Dsibe@users.noreply.github.com>
2019-08-23 18:36:42 +0300

    Update settings.py

--- org/settings.py
+++ org/settings.py
@@ -133,3 +133,8 @@ PAYPAL_TEST = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
