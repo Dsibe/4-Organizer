924404ab222f0cd7daea0c426c273449ccb558fe
Dsibe <darik.pc@gmail.com>
2019-10-10 20:20:42 +0300

    'views.py'

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    period = models.CharField(max_length=500, null=True, blank=True)
     def __str__(self):
         return self.user.username
 
