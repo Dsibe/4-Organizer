4181d46179f95bd5007ffff100bceda6526902e4
Dsibe <darik.pc@gmail.com>
2019-11-10 22:29:04 +0200

    'views.py'

--- main/views.py
+++ main/views.py
@@ -92,8 +92,10 @@ def main(request):
         if key.key:
             period = key.period
             if key.date:
-                date = datetime.datetime.strptime(key.date,
-                                                  '%Y-%m-%d')
+                try:
+                    date = datetime.datetime.strptime(key.date, '%Y-%m-%d %H:%M:%S.%')
+                except:
+                    date = datetime.datetime.strptime(key.date, '%Y-%m-%d')
                 date = add_months(date, int(period))
                 date = int_from_date(str(date))
                 if period == '0' or period == 0:
